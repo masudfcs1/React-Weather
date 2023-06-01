ca140b193b7763ecc61f34d76915e3b2edfaf363
masudfcs1 <masudfcs1@gmail.com>
2023-06-01 08:53:58 +0600

    added

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { UilSearch } from "@iconscout/react-unicons";
+const Input = () => {
+  return (
+    <div className=" flex flex-row justify-center my-6">
+      <div></div>
+    </div>
+  );
+};
+
+export default Input;
